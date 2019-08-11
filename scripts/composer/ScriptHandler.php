d6798ae9a0633bd6c2bf1af9fc1c96d90bb63629
zanvidmar <hello@zanvidmar.si>
2019-08-11 23:15:01 +0200

    Update script handing for sgt_installation_pofile

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -10,6 +10,7 @@ namespace DrupalProject\composer;
 use Composer\Script\Event;
 use Composer\Semver\Comparator;
 use DrupalFinder\DrupalFinder;
+use SgtInstallationProfile\SgtInstallationProfileScriptHandler;
 use Symfony\Component\Filesystem\Filesystem;
 use Webmozart\PathUtil\Path;
 
@@ -58,6 +59,26 @@ class ScriptHandler {
       $fs->mkdir($drupalRoot . '/sites/default/files', 0775);
       umask($oldmask);
       $event->getIO()->write("Created a sites/default/files directory with chmod 0775");
+    }
+
+    /**
+     * Prepare config install settings for sgt_profile_installation
+     *
+     * Copy all configuration from standard Drupal profile to
+     * sgt_installation_profile
+     */
+
+    // Check if sgt_installation_profile even exists
+    if ($fs->exists($drupalRoot . '/profiles/sgt/sgt_installation_profile')) {
+
+      // Check if sgt_installation_profile install configuration exist
+      if (!$fs->exists($drupalRoot . '/profiles/sgt/sgt_installation_profile/config')) {
+        SgtInstallationProfileScriptHandler::recurse_copy(
+          $drupalRoot . '/core/profiles/standard/config',
+          $drupalRoot . '/profiles/sgt/sgt_installation_profile/config'
+        );
+        $event->getIO()->write("Install configuration successfully cloned form Drupal standard installation profile.");
+      }
     }
   }
 
